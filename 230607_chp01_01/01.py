fc27a07c535d823eb0f138820f649a819dc57cde
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-20 10:53:49 +0900

    Add files via upload

---
+++ 230607_chp01_01/01.py
@@ -0,0 +1,6 @@
+# Hello World!
+print("반가워요!")
+
+i = input("숫자를 입력하세요")
+
+print(i)
---
+++ 230607_chp01_01/02_세미콜론.py
@@ -0,0 +1,7 @@
+# 기본 문법
+# 파이선파일 파일명 변경 단축키 shift + F6
+# 세미콜론(;)
+# 파이썬은 같은 줄에 여러 구문을 넣을 때만 사용
+
+print("Hello Python");
+print("Hello Python")
