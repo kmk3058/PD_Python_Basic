f8b8154c741f0d529badb6e821d99ed759404137
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-20 10:54:36 +0900

    Add files via upload

---
+++ 230608_chp01_02/self_practice.py
@@ -0,0 +1,33 @@
+
+# 조건문(if문)
+#
+# highTemperature = 28
+# lowTemperature = 20
+#
+# indoorTemperature = int(input("실내 온도 입력 : "))
+#
+# if indoorTemperature >= highTemperature:
+#     print('냉방 작동!')
+#
+# elif indoorTemperature <= lowTemperature:
+#     print('난방 작동!')
+
+# 양자택일 조건문(if~else)
+
+# seniorAge = 65
+#
+# passengerAge = int(input('나이 입력 : '))
+#
+# if passengerAge>= seniorAge:
+#     print('무료 대상 승객입니다.')
+#
+# else:
+#     print('유료 대상 승객입니다.')
+#
+# floatNum = float(input('소수 입력 : '))
+#
+# if floatNum - int(floatNum) >= 0.5:
+#     print(f'올림 : {}'.format(int(floatNum)+1))
+# else:
+#     print(f'내림 : {}'.format(int(floatNum)))
+
